2d3989a39c7518f08d643f97f6cd0d0547a4f9db
wuyufei123 <51846450+wuyufei123@users.noreply.github.com>
2020-09-08 16:39:53 +0800

    123

---
+++ snmp-plug/src/main/java/com/example/snmpplug/SnmpPlugApplication.java
@@ -0,0 +1,13 @@
+package com.example.snmpplug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmpPlugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnmpPlugApplication.class, args);
+    }
+
+}
