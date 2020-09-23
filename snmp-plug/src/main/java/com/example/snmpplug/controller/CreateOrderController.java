82b5270824ebb65607c7223f9d9b10c210b650ea
wuyufei123 <wuyufei@2020>
2020-09-23 16:01:25 +0800

    规则新增2

--- snmp-plug/src/main/java/com/example/snmpplug/controller/CreateOrderController.java
+++ snmp-plug/src/main/java/com/example/snmpplug/controller/CreateOrderController.java
@@ -2,16 +2,13 @@ package com.example.snmpplug.controller;
 
 import com.example.snmpplug.dto.DadaSource;
 import com.example.snmpplug.service.CreateOrderService;
-import com.example.snmpplug.utils.CreateOrderUtil;
-import net.sf.json.JSONObject;
-import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -22,12 +19,18 @@ import java.util.Map;
  * @Date 2020/9/15
  * @Version V1.0
  **/
-@Controller
+@RestController
 @RequestMapping("create")
 public class CreateOrderController {
     @Autowired
     CreateOrderService createOrderService;
 
+    /**
+     * 规则新增入库，规则新增snmp查询回显
+     *
+     * @param listDadaSource
+     * @return
+     */
     @PostMapping("SnmpCollect")
     public Map<String, List<String[]>> tes(@RequestBody DadaSource listDadaSource) {
         return createOrderService.insertOfCollect(listDadaSource);
